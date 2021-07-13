c4300253977b40982862c1df372c6017d8d381b4
boardengineer <77598527+boardengineer@users.noreply.github.com>
2021-07-13 01:37:13 -0700

    Add initial runhistory models

--- runhistory/admin.py
+++ runhistory/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import STSRun
+
 # Register your models here.
+admin.site.register(STSRun)
--- runhistory/models.py
+++ runhistory/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class STSRun(models.Model):
+	victory = models.BooleanField(default=False)
+	score = models.IntegerField(default=0)
