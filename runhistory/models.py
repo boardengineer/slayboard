97a3b47ddbe7b11592f78709cd43136e2fb1ad62
boardengineer <77598527+boardengineer@users.noreply.github.com>
2021-07-15 15:20:42 -0700

    Update Models and Serializers to add Decklists and relics

--- runhistory/models.py
+++ runhistory/models.py
@@ -1,11 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+# Contains the results of a single fun.
 class STSRun(models.Model):
 	victory = models.BooleanField(default=False)
 	score = models.IntegerField(default=0)
+	character_class = models.CharField(max_length=64, default='')
+	ascension = models.IntegerField(default=0)
 
+# Stats about a player (voter) for a single run
 class STSRunPlayer(models.Model):
 	run = models.ForeignKey(STSRun, related_name='players', on_delete=models.CASCADE)
 	screen_name = models.CharField(max_length=64)
 	votes = models.IntegerField(default=0)
+
+
+class Card(models.Model):
+	run = models.ForeignKey(STSRun, related_name='deck', on_delete=models.CASCADE)
+	card_id = models.CharField(max_length=64, default='')
+
+
+class Relic(models.Model):
+	run = models.ForeignKey(STSRun, related_name='relics', on_delete=models.CASCADE)
+	relic_id = models.CharField(max_length=64, default='')
--- runhistory/serializers.py
+++ runhistory/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import STSRun, STSRunPlayer
+from .models import STSRun, STSRunPlayer, Card, Relic
 
 class STSRunPlayerSerializer(serializers.ModelSerializer):
 	
@@ -8,16 +8,40 @@ class STSRunPlayerSerializer(serializers.ModelSerializer):
 		fields = ['screen_name', 'votes']
 
 
+class CardSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Card
+		fields = ['card_id']
+
+
+class RelicSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Relic
+		fields = ['relic_id']
+
+
 class STSRunSerializer(serializers.ModelSerializer):
 	players = STSRunPlayerSerializer(many=True)
+	deck = CardSerializer(many=True)
+	relics = RelicSerializer(many=True)
 
 	class Meta:
 		model = STSRun
-		fields = ['victory', 'score', 'players']
+		fields = ['victory', 'score', 'character_class', 'ascension', 'players', 'deck', 'relics']
 
 	def create(self, validated_data):
 		player_data = validated_data.pop('players')
+		deck_data = validated_data.pop('deck')
+		relic_data = validated_data.pop('relics')
 		run = STSRun.objects.create(**validated_data)
+
 		for player in player_data:
 			STSRunPlayer.objects.create(run=run, **player)
+
+		for card in deck_data:
+			Card.objects.create(run=run, **card)
+
+		for relic in relic_data:
+			Relic.objects.create(run=run, **relic)
+
 		return run
